42be38aa5e7658e1dbbbc07935378f0643a5a225
mohithemnani11 <69582028+mohithemnani11@users.noreply.github.com>
2024-11-26 15:40:32 +0530

    odin deployment setup on stag env (#6)
    * chore : added .odin & .config files
    
    * chore: updated release.yml
    
    * chore: made build.sh changes
    
    * chore: chnages in build.sh
    
    * chore: installed yarn
    
    * chore: switched server on prod
    
    * chore: changed script for build
    
    * fix: changed start.sh
    
    * fix: logging added
    
    * fix: changed node version
    
    * fix: changed node version
    
    * fix: nvm issue
    
    * Node version updated setup.sh
    
    * source removed from setup.sh
    
    * updated setup.sh
    
    * updated start.sh
    
    * nvm version removed
    
    * Updated package.json with 18.17.0 node version
    
    * Updated package.json having node 18.16.0
    
    * fix: tsx issue
    
    * fix: changed definition.json to 18.16.0
    
    * Update package.json : node version 18.18.0
    
    * fix: changed definition.json to 18.18.0
    
    ---------
    
    Co-authored-by: saurav0705 <sauravaggarwal98@gmail.com>

---
+++ .odin/cp-web-dashboard/setup.sh
@@ -0,0 +1,33 @@
+# export ENV=stag
+# echo "ENV: ${ENV}"
+# export SERVICE_NAME="cp-web-dashboard"
+# export NAMESPACE="master"
+# export CONFIG_BRANCH="master"
+# echo "SERVICE_NAME: ${SERVICE_NAME}"
+# echo "CONSUL_TOKEN: ${CONSUL_TOKEN}"
+# echo "VAULT_TOKEN: ${VAULT_TOKEN}"
+# echo "NAMESPACE: ${NAMESPACE}"
+# echo "CONFIG_BRANCH: ${CONFIG_BRANCH}"
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# source ~/.nvm/nvm.sh
+# nvm --version
+# nvm install  18.16.0
+# nvm use 18.16.0
+# node --version
+# npm cache clean --force
+# npm install -g pnpm
+# npm install -g yarn
+# yarn --version
+# pnpm install
+# pnpm --version
+corepack enable
+
+
+cd ${BASE_DIR}/${SERVICE_NAME}
+
+echo "Installing dependencies..."
+pnpm install
+
+NODE_ENV=production PORT=3000 pnpm build
